a57928d9956f848f8a9eb3b6e44f656d8932726c
Aiden McClelland <me@drbonez.dev>
2020-05-01 22:24:05 +0100

    0.2.3

--- build.sh
+++ build.sh
@@ -2,18 +2,9 @@
 
 appmgr rm --purge cups
 rm cups.s9pk
-cp ../cups${1}.tar.gz .
-echo "building cups${1}"
-rm -rf assets/www
-tar -C assets -xvf cups${1}.tar.gz
-#docker build --tag start9/cups .
-#docker save start9/cups > image.tar
-#docker rmi start9/cups
-echo "building 2 cups$"
+docker build --tag start9/cups .
+rm image.tar
+docker save start9/cups > image.tar
+docker rmi start9/cups
 appmgr -vv pack `pwd` -o `pwd`/cups.s9pk
 appmgr -vv install cups.s9pk
-echo "password: pass" | appmgr configure cups --stdin
-appmgr start cups
-appmgr tor show cups
-#rm ../cups${1}.tar.gz
-rm cups${1}.tar.gz
